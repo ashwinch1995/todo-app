81987888b0bc40e7a6a8f75f6aa76131ef22fb87
ashwinch1995 <acc9f@umsystem.edu>
2021-09-23 23:43:43 -0500

    Added animations, bootstrap and material icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
+import { trigger, transition, style, animate, keyframes } from '@angular/animations';
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations:[
+    trigger("moveInLeft",[
+     transition("void=> *",[style({transform:"translateX(300px)"}),
+      animate(200,keyframes([
+       style({transform:"translateX(300px)"}),
+       style({transform:"translateX(0)"})
+     ]))]),
+     transition("*=>void",[style({transform:"translateX(0px)"}),
+      animate(100,keyframes([
+       style({transform:"translateX(0px)"}),
+       style({transform:"translateX(300px)"})
+      ]))]) 
+     ])
+  ]
 })
 export class AppComponent {
   title = 'todo-app';
